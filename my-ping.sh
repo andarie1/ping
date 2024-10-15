b5c044cdc8328c21823d808a113f4091e440485c
Elena Ciobanu <elena.arhi88@gmail.com>
2024-10-15 18:04:57 +0300

    Create my-ping.sh
    DZ17-ping

---
+++ my-ping.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -e
+# Ввод адреса для пинга
+read -p "Введите IP адрес или доменное имя для пинга: " target
+# Переменные для подсчета неудачных пингов
+failed_count=0
+
+while true; do
+ping_result="$(ping -c 1 $target | awk -F 'time=' '/time=/ {print $2}' | cut -d' ')"
+ if [ -z "$ping_result" ]; then
+        # Если пинг не прошел
+        echo "Пинг неудачен"
+        ((fail_count++))
+    else
+            if (( $(echo "$ping_result > 100" | bc -l) )); then
+            echo "Время пинга превышает 100 мс: $ping_result мс"
+        else
+            echo "Пинг успешен: $ping_result мс"
+            failed_count=0
+        fi
+    fi
+
