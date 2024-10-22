2b469dc2df24bfcb9ed28c65049f20fdc611c0e2
Elena Ciobanu <elena.arhi88@gmail.com>
2024-10-22 11:58:15 +0300

    Update my-ping.sh
    Updated bash-script

--- my-ping.sh
+++ my-ping.sh
@@ -1,29 +1,29 @@
 #!/bin/bash
 
+#!/bin/bash
 set -e
-# Ввод адреса для пинга
-read -p "Введите IP адрес или доменное имя для пинга: " target
-# Переменные для подсчета неудачных пингов
-failed_count=0
 
- if [ -z "$ping_result" ]; then
-        # Если пинг не прошел
-        echo "Пинг неудачен"
-        ((fail_count++))
-    else
-            if (( $(echo "$ping_result > 100" | bc -l) )); then
-            echo "Время пинга превышает 100 мс: $ping_result мс"
-        else
-            echo "Пинг успешен: $ping_result мс"
-            failed_count=0
-        
-        fi
-    fi
-
-    if [ $fail_count -ge 3 ]; then
-        echo "Пинг не удается 3 раза подряд"
-        exit 1
-    fi
-
-    sleep 1
+target_address="google.com"
+max_ping_time_ms=10
+max_failures=3
+failures=0
+while true;
+ do
+ ping_result=$(ping -c 1 "$target_address" | grep 'icmp_seq')
+ ping_time=$(echo "$ping_result" | awk -F'=' '{print $4}' | awk '{print $1}' | awk -F'.' '{print $1}')
+ if [ -n "$ping_time" ] && [ "$ping_time" -gt "$max_ping_time_ms" ]; then
+ echo "Пинг $target_address превышает $max_ping_time_ms мс: $ping_time мс."
+ fi
+ if [ -z "$ping_time" ]; then
+   echo "Пинг $target_address не удался."
+   ((failures++))
+ else
+   echo "Пинг $target_address успешен: $ping_time мс."
+  failures=0
+ fi
+  if [ "$failures" -ge "$max_failures" ]; then
+   echo "Превышено максимальное количество неудачных попыток."
+  failures=0
+ fi
+ sleep 1
 done
